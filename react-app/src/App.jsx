15c584d0fb406ed1578bc23d4ccaa89e2fea9dba
arunbhalli <tomarbhalli@gmail.com>
2021-06-28 00:08:50 +0200

    adds functionality to show all courses

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import courses  from './data/courses';
 const App = () => {
   return (
-    <> 
-    <h1> COURSE LIST</h1>
-   
-    </>
-  )
-}
+    <div>
+      <h1> COURSE LIST</h1>
+      <div>
+        {courses.map((course) => (
+          <div>
+            <h3 data-cy='name'>{course.title}</h3>
+            <p data-cy='location'>{course.description}</p>
+            <p data-cy='car'><span className='bold'>Category: </span>{course.category}</p>
+            <p data-cy='age'><span className='bold'>Instructors: </span>{course.instructors}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
